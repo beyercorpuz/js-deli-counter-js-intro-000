89523f53fe9b6b8625a73ca11a322c043d9c7834
Chris Beyer <beyercorpuz@gmail.com>
2020-06-25 00:50:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var katzDeliLine = [];
 takeANumber(katzDeliLine, "name");
-'katzDeliLine' is currently ["Ada", "Grace"]
-return "Welcome, Ada. You are number 1 in line."
+//'katzDeliLine' is currently ["Ada", "Grace"]
+return "The line is currently: 1.Ada, Grace".
